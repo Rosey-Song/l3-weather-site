cb5f22b9d55a201e5317403fe664e9def2905d37
Lossi Kile <wiggina942@gmail.com>
2021-07-05 01:35:25 -0400

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ const path = require('path')
 // Require node libraries
 const express = require('express')
 const hbs = require('hbs')
+
 const app = express()
+const port = process.env['PORT'] || 3000
+
 
 // Setup local requirements
 const geocode = require('./utils/geocode')
@@ -109,6 +112,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
