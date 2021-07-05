84ddd8f79c09a216edbe021e79f34812f1d8f988
Lossi Kile <wiggina942@gmail.com>
2021-07-05 02:14:22 -0400

    Add visibility

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 module.exports = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=d592f83d91246d8b4c3b06c37688b9fb&query=' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=d592f83d91246d8b4c3b06c37688b9fb&query=' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '&units=m'
     request({url, json: true}, (err, res) => {
         if (err) {
             callback('Could not connect to weather service', undefined)
@@ -10,8 +10,20 @@ module.exports = (latitude, longitude, callback) => {
             callback('Could not find location', undefined)
         }
         else {
-            const {temperature, feelslike, weather_descriptions} = res['body']['current']
-            callback(undefined, weather_descriptions[0] + '. It is currently ' + temperature + ' degrees out.  It feels like ' + feelslike + ' degrees out.')
+            const {temperature, feelslike, weather_descriptions, is_day, visibility} = res['body']['current']
+
+            const daytime = is_day === 'yes' ? 'day' : 'night'
+            const vis = (visibility) => {
+                if (visibility > 10)
+                    return 'Clear'
+                else if (visibility > 5)
+                    return 'High'
+                else if (visibility > 1)
+                    return 'Low'
+                else
+                    return 'Very low'
+            }
+            callback(undefined, weather_descriptions[0] + '. It is currently ' + temperature + ' degrees out.  It feels like ' + feelslike + ' degrees out to' + daytime + '. ' + vis(visibility) + ' visibility at the moment.')
         }
     })
 }
